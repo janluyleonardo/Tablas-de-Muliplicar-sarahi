bd57649eb1e440f346ac200c62dafb637f11cdc2
janluyleonardo <janluy.moreno@gmail.com>
2025-08-12 18:18:31 -0500

    carga de archivos adicionales

---
+++ src/login/main.java
@@ -0,0 +1,13 @@
+package login;
+
+public class main {
+
+    public static void main(String[] args) {
+
+        inisio_sesion inicio = new inisio_sesion();
+        inicio.setVisible(true);
+    }
+
+    
+
+}
